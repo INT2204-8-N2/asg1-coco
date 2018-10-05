1ddc0a7034c5519959ed69cd37e52d7e35403fb9
Nguyễn Thị Vân Anh <van005618@gmail.com>
2018-10-05 15:13:51 +0700

    verson 3.1

---
+++ DictionaryCOCO/src/dictionarycoco/DictionaryComandLine.java
@@ -0,0 +1,18 @@
+
+package dictionarycoco;
+
+public class DictionaryComandLine {
+    public void dictionaryBasic(){
+        DictionaryManagement dm = new DictionaryManagement();
+//        dm.insertFromFile();
+//        dm.showAllWorlds();
+//        dm.dictionaryLookUp();
+//        dm.dictionarySearcher();
+//        dm.addword();
+//        dm.dictionaryExportToFile("output");
+    }
+    public static void main(String [] args){
+        DictionaryComandLine dcl = new DictionaryComandLine();
+        dcl.dictionaryBasic();
+    }
+}
