293685ed123d4040ca82d4049363969c0b86972d
Admin <Admin@Admin-PC>
2018-10-10 09:48:55 +0700

    BTL 3.3 test API

---
+++ DictionaryCOCO/src/API/HelloAPI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package API;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class HelloAPI {
+    public static void main(String[] args) {
+        try {
+            System.out.println(GoogleTranslate.translate("vi", "I a exo lover"));
+        } catch (IOException ex) {
+            Logger.getLogger(HelloAPI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+ 
+      
+}
