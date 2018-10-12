52f0aa851402f4cb353a8ea513395cf0cdba6da3
Nguyễn Thị Vân Anh <van005618@gmail.com>
2018-10-12 19:51:23 +0700

    add history and speech to text

--- DictionaryCOCO/src/API/DictionaryApplication.java
+++ DictionaryCOCO/src/API/DictionaryApplication.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package JFrameApplication;
+package API;
 
+import JFrameApplication.*;
 import API.SynthesiserV2;
 import Dtb.DTB;
 import dictionarycoco.Word;
@@ -506,6 +507,7 @@ public class DictionaryApplication extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(DictionaryApplication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
